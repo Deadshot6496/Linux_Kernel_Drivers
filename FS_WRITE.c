a555010e45a9c0ce716d7801d84be3f5ba4dd376
DeadShot <40751180+Deadshot6496@users.noreply.github.com>
2025-11-22 23:17:09 +0530

    Update FS_WRITE.c

--- FS_WRITE.c
+++ FS_WRITE.c
@@ -1,74 +1,190 @@
+/***************************************************************************//**
+*  \file       myModule.c
+*
+*  \details    Simple hello world driver
+*
+*  \author     Karthik
+*
+* *******************************************************************************/
+#include<linux/kernel.h>
+#include<linux/init.h>
+#include<linux/module.h>
+#include<linux/moduleparam.h>
+#include<linux/fs.h>
+#include<linux/kdev_t.h>
+#include<linux/device.h>
+#include<linux/err.h>
+#include<linux/cdev.h>
+
+int valueETX, arr_valueETX[4];
+char *nameETX;
+int cb_valueETX = 0;
 
 
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
 
-int main() {
-    
-	// Create a new file or open it
-	//if it exists (write-only access)
-	// File permissions: rw-r--r--
-	
-	char *value = "100";
-	int fd = open("/sys/module/myModule/parameters/cb_valueETX", O_RDWR);
-	int fd_1 = open("/dev/MY_DEVICE", O_RDONLY);
-	int sizeRead = 0;
-	char buffer_read[256];
-	if(fd_1 == -1)
-	{
-		perror("Error opening DEVICE FILE");
-		close(fd);
-		close(fd_1);
+module_param(valueETX, int, S_IRUSR|S_IWUSR);                      //integer value
+module_param(nameETX, charp, S_IRUSR|S_IWUSR);                     //String
+module_param_array(arr_valueETX, int, NULL, S_IRUSR|S_IWUSR);      //Array of integers
 
-		return -1;
-	}
-	if (fd == -1) {
-		perror("Error opening file");
-		close(fd);
-		close(fd_1);
-
-		return 1;
-	}
-
-	write(fd, value, strlen(value));
-	sizeRead = read(fd_1,buffer_read,sizeof(buffer_read)-1);
-	printf("%s\n",buffer_read);
-	if(sizeRead == -1)
-	{
-		perror("error reading DEVICE file");
-		close(fd_1);
-		close(fd);
-
-		return -1;
-	}
-	// Close the file descriptor
-	close(fd);
-
-	return 0;
-} 
-
-/*#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-int main ()
+static int      etx_open(struct inode *inode, struct file *file);
+static int      etx_release(struct inode *inode, struct file *file);
+static ssize_t  etx_read(struct file *filp, char __user *buf, size_t len,loff_t * off);
+static ssize_t  etx_write(struct file *filp, const char *buf, size_t len, loff_t * off);
+static struct file_operations fops =
 {
-	printf("Started");
-	printf "Value: %s\n" "77" >> /sys/module/myModule/parameters/cb_valueETX.txt;
-	int f = open("/sys/module/myModule/parameters/cb_valueETX", O_RDWR);
-	if(f == 1)
-	{
-		printf("ERROR");
-	}
+    .owner      = THIS_MODULE,
+    .read       = etx_read,
+    .write      = etx_write,
+    .open       = etx_open,
+    .release    = etx_release,
+};
 
-	if(write(f, "77", 2) == 1)
-	{
-		printf("ERROR");
-	}
-	close(f);
+
+
+ 
+/*----------------------Module_param_cb()--------------------------------*/
+int notify_param(const char *val, const struct kernel_param *kp)
+{
+        int res = param_set_int(val, kp); // Use helper for write variable
+        if(res==0) {
+                printk(KERN_INFO "Call back function called...\n");
+                printk(KERN_INFO "New value of cb_valueETX = %d\n", cb_valueETX);
+                return 0;
+        }
+        else 
+        {
+           int test = cb_valueETX + 1;
+           printk(KERN_INFO "NEW VALUE TESTER %d\n",test);
+        }
+        return -1;
 }
+ 
+const struct kernel_param_ops my_param_ops = 
+{
+        .set = &notify_param, // Use our setter ...
+        .get = &param_get_int, // .. and standard getter
+};
+ 
+module_param_cb(cb_valueETX, &my_param_ops, &cb_valueETX, S_IRUSR|S_IWUSR ); //IRUGO
+/*-------------------------------------------------------------------------*/
+
+char data[128]; // Holding the data buffer for the device file.
+
+
+/*
+** This function will be called when we open the Device file
 */
+static int etx_open(struct inode *inode, struct file *file)
+{
+        pr_info("Driver Open Function Called...!!!\n");
+        return 0;
+}
+/*
+** This function will be called when we close the Device file
+*/
+static int etx_release(struct inode *inode, struct file *file)
+{
+        pr_info("Driver Release Function Called...!!!\n");
+        return 0;
+}
+/*
+** This function will be called when we read the Device file
+*/
+static ssize_t etx_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
+{
+        pr_info("Driver Read Function Called...!!!\ Offset -> %d\n",*off);
+	int to_copy = (len + *off) < sizeof(data) ? len : (sizeof(data)-*off);
+	if(*off >= sizeof(data))
+	{
+		return 0;
+	}
+	int not_copied = copy_to_user(buf,&data[*off],to_copy);
+	pr_info("Buffer = %s\n",buf);
+	int delta = to_copy-not_copied;
+	pr_warn("Not copied count = %d", delta);
+	*off += delta;
+
+        return delta;
+}
+/*
+** This function will be called when we write the Device file
+*/
+static ssize_t etx_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
+{
+        pr_info("Driver Write Function Called...!!! Offset -> %d\n",*off);
+	int to_copy = (len + *off) < sizeof(data) ? len : (sizeof(data)-*off);
+	if(*off >= sizeof(data))
+	{
+		return 0;
+	}
+	int not_copied = copy_from_user(&data[*off],buf,to_copy);
+	int delta = to_copy-not_copied;
+	pr_warn("Not copied count = %d", delta);
+	*off += delta;
+        return delta;
+}
+
+dev_t dev =0;  // 32 bit integer where first 12 bits are major and remaining 20 bits are minor Number
+static struct class *dev_class;
+static struct cdev etx_cdev;
+/*
+** Module init function
+*/
+static int __init hello_world_init(void)
+{
+        int i;
+        printk(KERN_INFO "ValueETX = %d  \n", valueETX);
+        printk(KERN_INFO "cb_valueETX = %d  \n", cb_valueETX);
+        printk(KERN_INFO "NameETX = %s \n", nameETX);
+        for (i = 0; i < (sizeof arr_valueETX / sizeof (int)); i++) {
+                printk(KERN_INFO "Arr_value[%d] = %d\n", i, arr_valueETX[i]);
+        }
+        printk(KERN_INFO "Kernel Module Inserted Successfully...\n");
+	if((alloc_chrdev_region(&dev,6,1,"Ext_Device")>0))
+	{
+		printk("Allocation of major number issue\n");
+		return -1;
+	}
+	pr_info("Major = %d, Minor = %d\n",MAJOR(dev),MINOR(dev));
+                /*Creating cdev structure*/
+        cdev_init(&etx_cdev,&fops);
+        /*Adding character device to the system*/
+        if((cdev_add(&etx_cdev,dev,1)) < 0){
+            pr_err("Cannot add the device to the system\n");
+            goto r_class;
+       }
+
+	dev_class = class_create("My_Char_Device");
+	if(IS_ERR(dev_class)){
+	 goto r_class;
+	}
+	if(IS_ERR(device_create(dev_class,NULL,dev,NULL, "My_Char_Device"))){
+	 goto r_device;
+	}
+	pr_info("kernel device creation success \n ");
+        return 0;
+
+r_device: class_destroy(dev_class);
+r_class: unregister_chrdev_region(dev,1); return -1;
+
+}
+
+/*
+** Module Exit function
+*/
+static void __exit hello_world_exit(void)
+{
+    device_destroy(dev_class,dev);
+    class_destroy(dev_class);
+    unregister_chrdev_region(dev,1);
+    printk(KERN_INFO "Kernel Module Removed Successfully...\n");
+
+}
+ 
+module_init(hello_world_init);
+module_exit(hello_world_exit);
+ 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("EmbeTronicX <embetronicx@gmail.com>");
+MODULE_DESCRIPTION("A simple hello world driver");
+MODULE_VERSION("1.0");
